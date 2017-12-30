fbed007fd6bc8085d6eabecdd1e57aab7af358c6
Rémi Nivet <rnivet@gmail.com>
2017-12-30 16:47:50 +0100

    Add filter to ignore members without proper photo

--- scripts/export_members.py
+++ scripts/export_members.py
@@ -42,12 +42,19 @@ openerp, uid, tz = init_openerp(
 ###############################################################################
 
 output_dir = "/var/www/html/sqq/data/membres"
+#output_dir = "./"
 
 # List members
 try:
     out_members = []
-    members = openerp.ResPartner.browse([], limit=10)
+    members = openerp.ResPartner.browse([], limit=24)
     for member in members:
+        # Check if member has photo uploaded in Odoo
+        if (len(member.image) < 100000):
+            print member.name, "excluded because photo is to small"
+            continue
+
+        # Add member info to output list
         (name, surname) = member.name.split(',')
         out_members.append(
                 {
@@ -58,12 +65,15 @@ try:
                 }
         )
 
-        img_file = open("%s/data/identity/%s.png" % (output_dir, str(member.barcode_base)), 'w')
+        # Store member photo
+        img_file = open("%s/data/photos/%s.png" % (output_dir, str(member.barcode_base)), 'w')
         img_file.write(base64.b64decode(member.image))
         img_file.close()
 
+    # Create output json
     data_file = open("%s/data/membres.json" % (output_dir), 'w')
     json.dump(out_members, data_file, indent=4)
     data_file.close()
+
 except Exception as e:
     print e
