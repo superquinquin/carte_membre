45f58cafdd747f805f3a08ca871140be4a80c732
Rémi Nivet <rnivet@gmail.com>
2018-01-23 08:52:49 +0100

    Ajout d'un filtre pour ignorer les membres dont le nom ne contient pas une ',' Export dans 2 fichiers json spepares les membres avec photo et sans photo.

--- scripts/export_members.py
+++ scripts/export_members.py
@@ -37,45 +37,58 @@ openerp, uid, tz = init_openerp(
 # Configuration
 ###############################################################################
 
+output_dir = "/var/www/html/sqq/data/membres"
+#output_dir = "/home/ben/git/sqq/carte_membre/html"
+#output_dir = "./"
+
 ###############################################################################
 # Script
 ###############################################################################
 
-#output_dir = "/var/www/html/sqq/data/membres"
-output_dir = "/home/ben/git/sqq/carte_membre/html"
+def add_member_to_list(member, member_list):
+    (name, surname) = member.name.split(',', 1)
+    member_list.append(
+            {
+            "num": member.barcode_base,
+            "name": name,
+            "surname": surname,
+            "barcode": member.barcode,
+            "barcode2":member.barcode[:-1],
+            "sex": member.sex
+            }
+    )
 
 # List members
 try:
-    out_members = []
+    nb_member = 0
+    members_with_photo = []
+    members_without_photo = []
     members = openerp.ResPartner.browse([('is_worker_member', '=', 'true')])
     for member in members:
-        # Check if member has photo uploaded in Odoo
-        if (len(member.image) < 100000):
-            print member.name, "excluded because photo is to small"
+        if ',' not in member.name:
             continue
+        # Check if member has photo uploaded in Odoo
+        if (len(member.image) > 100000):
+            # Add member info to output list
+            add_member_to_list(member, members_with_photo)
+            # Store member photo
+            img_file = open("%s/data/photos/%s.jpg" % (output_dir, str(member.barcode_base)), 'w')
+            img_file.write(base64.b64decode(member.image))
+            img_file.close()
+        else:
+            add_member_to_list(member, members_without_photo)
 
-        # Add member info to output list
-        (name, surname) = member.name.split(',')
-        out_members.append(
-                {
-                "num": member.barcode_base,
-                "name": name,
-                "surname": surname,
-                "barcode": member.barcode,
-                "barcode2":member.barcode[:-1],
-                "sex": member.sex
-                }
-        )
-
-        # Store member photo
-        img_file = open("%s/data/photos/%s.png" % (output_dir, str(member.barcode_base)), 'w')
-        img_file.write(base64.b64decode(member.image))
-        img_file.close()
+        print "Data extracted for ", member.name
+        nb_member = nb_member+1
 
     # Create output json
-    data_file = open("%s/data/membres.json" % (output_dir), 'w')
-    json.dump(out_members, data_file, indent=4)
+    data_file = open("%s/data/membres_photo.json" % (output_dir), 'w')
+    json.dump(members_with_photo, data_file, indent=4)
     data_file.close()
+    data_file = open("%s/data/membres_nophoto.json" % (output_dir), 'w')
+    json.dump(members_without_photo, data_file, indent=4)
+    data_file.close()
+    print "Total: %d members exported from Odoo" % (nb_member)
 
 except Exception as e:
     print e
