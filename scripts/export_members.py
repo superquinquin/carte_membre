c1bf71c8abfad8797e3fcc84662554d68b0d865f
Remi Nivet <rnivet@gmail.com>
2018-03-14 21:19:53 +0100

    feat(export_members): export only members with badge to print feat(export_members): add option to mark exported members as printed refactor(export_members): output dir is now set in cmd args

--- scripts/export_members.py
+++ scripts/export_members.py
@@ -6,13 +6,13 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+import os
+import argparse
+import logging
 import datetime
-import time
 import erppeek
 import json
 import base64
-
-from datetime import date
 
 from cfg_secret_configuration import odoo_configuration_user_prod as user_config
 
@@ -37,10 +37,6 @@ openerp, uid, tz = init_openerp(
 # Configuration
 ###############################################################################
 
-#output_dir = "/var/www/html/sqq/data/membres"
-output_dir = "/home/ben/git/sqq/carte_membre/html"
-#output_dir = "./"
-
 ###############################################################################
 # Script
 ###############################################################################
@@ -58,38 +54,79 @@ def add_member_to_list(member, member_list):
             }
     )
 
-# List members
-try:
+def save_json(file_name, data):
+    data_file = open(file_name, 'w')
+    json.dump(data, data_file, indent=4)
+    data_file.close()
+    logging.debug("Data saved to json file %s", file_name)
+
+def main():
+    # configure arguments parser
+    parser = argparse.ArgumentParser(
+            description='Export des membres dont la carte doit être imprimée')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-m', '--mark-as-printed', action='store_true',
+            help="mark members as printed (warning this can't be undone)")
+    parser.add_argument('output_dir', metavar='OUTPUT_DIR',
+            help='the output directory')
+    args = parser.parse_args()
+
+    # configure logger
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    today = datetime.date.today().isoformat()
     nb_member = 0
     members_with_photo = []
     members_without_photo = []
-    members = openerp.ResPartner.browse([('is_worker_member', '=', 'true')])
-    for member in members:
-        if ',' not in member.name:
-            continue
-        # Check if member has photo uploaded in Odoo
-	# la photo du membre 69 fait 65391, on passe à 50000
-        if (len(member.image) > 50000):
-            # Add member info to output list
-            add_member_to_list(member, members_with_photo)
-            # Store member photo
-            img_file = open("%s/data/photos/%s.jpg" % (output_dir, str(member.barcode_base)), 'w')
-            img_file.write(base64.b64decode(member.image))
-            img_file.close()
-        else:
-            add_member_to_list(member, members_without_photo)
+    try:
+        # Create output dirs
+        if not os.path.isdir("%s/photos" % (args.output_dir)):
+            os.makedirs("%s/photos" % (args.output_dir))
+        # List members
+        members = openerp.ResPartner.browse(
+                [('is_worker_member', '=', 'true'),
+                    ('badge_to_print', '=', 'true')])
+        for member in members:
+            if ',' not in member.name:
+                continue
+            # Check if member has photo uploaded in Odoo
+            if (len(member.image) > 50000):
+                logging.debug("Found member with photo: %s", member.name)
+                add_member_to_list(member, members_with_photo)
+                # Store member photo
+                img_file_name = "%s/photos/%s.jpg" % (args.output_dir,
+                        str(member.barcode_base))
+                img_file = open(img_file_name, 'w')
+                img_file.write(base64.b64decode(member.image))
+                img_file.close()
+                logging.debug("Member photo saved to file %s", img_file_name)
+            else:
+                logging.debug("Found member without photo: %s", member.name)
+                add_member_to_list(member, members_without_photo)
 
-        print "Data extracted for ", member.name
-        nb_member = nb_member+1
+            logging.info("Data extracted for [%s]", member.name)
+            nb_member = nb_member+1
 
-    # Create output json
-    data_file = open("%s/data/membres_photo.json" % (output_dir), 'w')
-    json.dump(members_with_photo, data_file, indent=4)
-    data_file.close()
-    data_file = open("%s/data/membres_nophoto.json" % (output_dir), 'w')
-    json.dump(members_without_photo, data_file, indent=4)
-    data_file.close()
-    print "Total: %d members exported from Odoo" % (nb_member)
+            # Mark member as printed if asked to
+            if args.mark_as_printed:
+                member.badge_print_date = today
+                member.badge_to_print = False
+                logging.debug("Member [%s] marked as printed to %s",
+                        member.name, today)
 
-except Exception as e:
-    print e
+        # Create output json
+        save_json("%s/membres_photo.json" % (args.output_dir),
+                members_with_photo)
+        save_json("%s/membres_nophoto.json" % (args.output_dir),
+                members_without_photo)
+        print "Total: %d members exported from Odoo" % (nb_member)
+
+
+    except Exception as e:
+        logging.exception(e)
+
+if __name__ == "__main__":
+    main()
